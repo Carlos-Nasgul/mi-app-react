70e42d3da62bdd41db1e6f78ca5ac2156688b093
Carlos-Nazgul <carlosgarciarico1974@gmail.com>
2024-06-06 12:13:52 +0200

    cambios en archi y borrado archivos

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,24 +1,27 @@
 //PRIMER COMPONENTE
 
 //import React from 'react'; YA NO HACE FALTA DESDE 2020
-import '../hojas-de-estilo/Testimonio.css'
-
+import "../hojas-de-estilo/Testimonio.css";
 
 function Testimonio(props) {
-    return (
-        <div className='contenedor-testimonio'>
-            <img className='imagen-testimonio' src={require(`../imagenes/${props.imagen}.png`)} alt='foto de perfil' />
-            <div className='contenido-testimonio'>
-                <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
-                <p className='cargo-testimonio'>{props.cargo}</p>
-                <p className='texto-testimonio'>"{props.testimonio}"</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require(`../imagenes/${props.imagen}.png`)}
+        alt="foto de perfil"
+      />
+      <div className="contenido-testimonio">
+        <p className="nombre-testimonio">
+          {props.nombre} en {props.pais}
+        </p>
+        <p className="cargo-testimonio">{props.cargo}</p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
 }
 
 export default Testimonio;
 
-console.log('Hola React');
-
-
+console.log("Hola React");
